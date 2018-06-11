b6ac7818e96342926f4c579a8e0d3979661e9c34
lipengzhu1996 <lipeng_zhu@outlook.com>
2018-06-11 11:04:49 +0800

    update

--- readimg.py
+++ readimg.py
@@ -6,12 +6,23 @@ rootdir ='/home/hadoop/workspace/imgs'
 file = open('/home/hadoop/workspace/data.txt','w')
 list = os.listdir(rootdir)
 
+index = 1
+
+target = cv.imread('/home/hadoop/workspace/target.jpg',cv.IMREAD_GRAYSCALE)
+target = cv.resize(target,(139,175))
+file.write('00')
+file.write('\t')
+file.write(str(target.reshape(1,-1).tolist()[0]))
+file.write('\n')
 
 for i in range(0,len(list)):
     path = os.path.join(rootdir,list[i])
     if os.path.isfile(path):
         img = cv.imread(path,cv.IMREAD_GRAYSCALE)
-        img = cv.resize(img,(512,512))
+        img = cv.resize(img,(320,240))
+
+        file.write(list[i][:2])
+        file.write('\t')
         file.write(str(img.reshape(1,-1).tolist()[0]))
         file.write('\n')
 
